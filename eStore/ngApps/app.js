625f0d21da91f6fcba479701d5e33c609e9ddb92
Marisa3894 <marisa.reilly@outlook.com>
2015-07-05 18:06:36 -0700

    CRUD pages

--- eStore/ngApps/app.js
+++ eStore/ngApps/app.js
@@ -18,6 +18,37 @@
                     controllerAs: 'main'
                 })
 
+                .when('/list', {
+                    templateUrl: '/ngViews/list.html',
+                    controller: 'ListController',
+                    controllerAs: 'main'
+                })
+
+                .when('/add', {
+                    templateUrl: '/ngViews/add.html',
+                    controller: 'AddController',
+                    controllerAs: 'main'
+                })
+
+                .when('/details/:id', {
+                    templateUrl: '/ngViews/details.html',
+                    controller: 'DetailsController',
+                    controllerAs: 'main'
+                })
+
+                .when('/edit/:id', {
+                    templateUrl: '/ngViews/edit.html',
+                    controller: 'EditController',
+                    controllerAs: 'main'
+                })
+
+                .when('/delete/:id', {
+                    templateUrl: '/ngViews/delete.html',
+                    controller: 'DeleteController',
+                    controllerAs: 'main'
+                })
+
+
             .otherwise({ redirectTo: '/' });
 
             $locationProvider.html5Mode(true);
--- eStore/ngApps/controllers.js
+++ eStore/ngApps/controllers.js
@@ -8,4 +8,62 @@
 
     });
 
+    angular.module('StoreApp').controller('ListController', function (PROD_API, $resource, $location) {
+        var self = this;
+
+        var Product = $resource(PROD_API);
+        self.products = Product.query();
+    });
+
+    angular.module('StoreApp').controller('AddController', function (PROD_API, $resource, $routeParams, $location) {
+        var self = this;
+
+        var Product = $resource(PROD_API);
+        self.add = function () {
+            var newProduct = new Product(self.newProduct);
+            newProduct.$save(function () {
+                $location.path('/list');
+            });
+        }
+    });
+
+    angular.module('StoreApp').controller('DetailsController', function (PROD_API, $resource, $routeParams, $location) {
+        var self = this;
+
+        var Product = $resource(PROD_API);
+        self.product = Product.get({ id: $routeParams.id });
+    });
+
+    angular.module('StoreApp').controller('EditController', function (PROD_API, $resource, $routeParams, $location) {
+        var self = this;
+
+        var Product = $resource(PROD_API);
+        self.product = Product.get({ id: $routeParams.id });
+
+        self.edit = function () {
+            self.product.$save(function () {
+                $location.path('/list');
+            });
+        }
+    });
+
+    angular.module('StoreApp').controller('DeleteController', function (PROD_API, $resource, $routeParams, $location) {
+        var self = this;
+        var Product = $resource(PROD_API);
+        self.product = Product.get({ id: $routeParams.id });
+
+        self.remove = function () {
+            Product.remove({ id: self.product.id }).$promise.then(function () {
+                $location.path('/list');
+            }, function () {
+                //alert(['Product deletion failed.'])
+            });
+        }
+
+        self.redirect = function () {
+            $location.path('/list');
+        }
+    });
+
+
 })();
